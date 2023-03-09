0110ec55a4a2c69469ee94f17181a1ece41b8b71
serhatprogramming <serhatprogramming@gmail.com>
2023-03-09 17:50:59 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <>Hello</>;
+  const [value, setValue] = useState("");
+  const [rates, setRates] = useState({});
+  const [currency, setCurrency] = useState(null);
+
+  useEffect(() => {
+    console.log("effect run, currency is now", currency);
+
+    // skip if currency is not defined
+    if (currency) {
+      console.log("fetching exchange rates...");
+      axios
+        .get(`https://open.er-api.com/v6/latest/${currency}`)
+        .then((response) => {
+          setRates(response.data.rates);
+        });
+    }
+  }, [currency]);
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    setCurrency(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        currency:{" "}
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type="submit">exchange rate</button>
+      </form>
+      <pre>{JSON.stringify(rates, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
